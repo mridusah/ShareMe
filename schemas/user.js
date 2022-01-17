9eb61823d41d0c56919fed48d07422543f095f0d
Mridu Sah <54724473+mridusah@users.noreply.github.com>
2022-01-17 12:11:24 +0530

    Add files via upload

---
+++ schemas/user.js
@@ -0,0 +1,17 @@
+export default{
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userNAme',
+            title: 'UserName',
+            type: 'string',
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string',
+        },
+    ]
+}
